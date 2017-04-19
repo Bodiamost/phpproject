942ac784210da72c92734e91a1f04dad64552e3f
Bodiamost <bodiamost@gmail.com>
2017-04-19 14:06:50 -0400

    Rss try one

--- features/rssfeed/index.php
+++ features/rssfeed/index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>News Feed</title>
-    <link rel="stylesheet" href="css/style.css" />
-</head>
 
-<body>
     <div class="container">
         <div class="header">
             <img src="images/heade.jpg" width="440px" height="100px"/>
@@ -19,5 +11,3 @@
             Powered by <a href="http://www.Bohdanmost.com/network">Network.com</a>
         </div>
     </div>
-</body>
-</html>
